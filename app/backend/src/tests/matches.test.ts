14c0f3c9e7ece6f2f18c8571e2f1ff27cd0e13f7
Daniel Oliveira <danoliveira865@gmail.com>
2023-07-21 20:12:23 -0300

    Adiciona testes

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -3,7 +3,7 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 import SequelizeMatches from '../database/models/SequelizeMatches'
-import { matchesList } from './mocks/matchesMock';
+import { matchesList, filteredList } from './mocks/matchesMock';
 
 import { App } from '../app';
 
@@ -21,11 +21,17 @@ describe('Teams teste', () => {
   })
 
   describe('Requisição com sucesso', () => {
-    it.only('O endpoint deve retornar a lista de partidas corretamente', async () => {
+    it('O endpoint deve retornar a lista de partidas corretamente', async () => {
       sinon.stub(SequelizeMatches, 'findAll').resolves(matchesList as any);
       const { status, body } = await chai.request(app).get('/matches');
       expect(status).to.equal(200);
       expect(body).to.deep.equal(matchesList);
     });
+    it.only('O endpoint retorna a lista de partidas de acordo com o filtro inProgress', async () => {
+      sinon.stub(SequelizeMatches, 'findAll').resolves(filteredList as any);
+      const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+      expect(status).to.equal(200);
+      expect(body).to.deep.equal(filteredList);
+    })
   });
 })
